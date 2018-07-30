1edcc5a29d824b5cf9afe4cb13bb558c05f4014f
ayebo <31093794+ayebo@users.noreply.github.com>
2018-07-30 23:39:02 +0800

    Practical exercises

---
+++ Practical04/src/EvenOdd.java
@@ -0,0 +1,27 @@
+//EvenOdd.java
+import java.util.Scanner;
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.print("Enter a number--");
+        number = input.nextInt();
+        if (isEven(number))
+            System.out.println(number + "is Even.");
+        else
+            System.out.println(number + "is Odd.");
+    }
+    public static boolean isEven(int number)
+    {
+        boolean result;
+        if (number % 2 == 1)
+            result = false;
+        else
+        {
+            result = true;
+        }
+        return result;
+    }
+}
